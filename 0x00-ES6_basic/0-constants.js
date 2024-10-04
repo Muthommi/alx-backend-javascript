6b621cbece016c0760a2488fdd0081bbcaac4d5b
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-10-04 09:07:12 +0300

    const or let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,12 @@
+var lastPart = getLast();
+
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
+}
+
+export function getLast() {
+  return ' is okay';
 }
 
 export function taskNext() {
