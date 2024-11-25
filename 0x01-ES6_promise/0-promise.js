47a013b00edc6fe9ed88e3cbf475d6ab86572dd3
Muthommi <kelvinmartinmuthomi@gmail.com>
2024-11-25 09:32:18 +0000

    Keep every promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/**
+ * Returns array of objects representing students.
+ * @returns {Array} Array of student objects.
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
